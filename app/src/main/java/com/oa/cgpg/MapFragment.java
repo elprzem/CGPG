3332d6ef091a8b7cb92737e3d4972fd29f06b7ea
Przemysław Farion <przefar@gmail.com>
2014-10-13 19:47:46 +0200

    Dodanie mapImageView do layout'u MapFragment.

--- app/src/main/java/com/oa/cgpg/MapFragment.java
+++ app/src/main/java/com/oa/cgpg/MapFragment.java
@@ -1,25 +1,18 @@
 package com.oa.cgpg;
 
-import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MapFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MapFragment#newInstance} factory method to
- * create an instance of this fragment.
+/** Fragment containing map
  *
  */
 public class MapFragment extends Fragment {
+
+    private ImageView mapImageView;
 
     public MapFragment() {
         // Required empty public constructor
@@ -34,8 +27,11 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        mapImageView = (ImageView) view.findViewById(R.id.mapImageView);
+        
+        return view;
     }
 
 }
