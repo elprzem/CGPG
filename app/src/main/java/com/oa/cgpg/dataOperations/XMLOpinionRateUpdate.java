faf8d83f96241bff61eb0ea7c78e8ff47ffe9030
tomuszom <tomuszom@gmail.com>
2014-11-25 00:17:59 +0100

    update poiRates w locie

---
+++ app/src/main/java/com/oa/cgpg/dataOperations/XMLOpinionRateUpdate.java
@@ -0,0 +1,7 @@
+package com.oa.cgpg.dataOperations;
+
+/**
+ * Created by Tomasz on 2014-11-24.
+ */
+public class XMLOpinionRateUpdate {
+}
---
+++ app/src/main/java/com/oa/cgpg/models/opinionRatingUpdateNet.java
@@ -0,0 +1,7 @@
+package com.oa.cgpg.models;
+
+/**
+ * Created by Tomasz on 2014-11-24.
+ */
+public class opinionRatingUpdateNet {
+}
